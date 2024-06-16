9a9e025aba637273e1d6d2ed3058ef1939774731
jangjinhong <honge1122@naver.com>
2024-06-16 17:38:39 +0900

    Initial commit of Spring Boot project with JPA, MySQL, etc. configuration

---
+++ src/main/java/com/example/swyp_team1_back/SwypTeam1BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.swyp_team1_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwypTeam1BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwypTeam1BackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/swyp_team1_back/SwypTeam1BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swyp_team1_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwypTeam1BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
