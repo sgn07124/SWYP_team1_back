e6273721ea3b945f8d768fe77694903c47e266ab
Kim Kyeongho <sgn07124@gmail.com>
2024-07-02 22:24:10 +0900

    BaseEntity, BaseTimeEntity 생성

---
+++ src/main/java/com/example/swyp_team1_back/global/common/entity/BaseEntity.java
@@ -0,0 +1,19 @@
+package com.example.swyp_team1_back.global.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity extends BaseTimeEntity {  // 작성자 / 작성 시간, 수정시간
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createBy;
+
+}
