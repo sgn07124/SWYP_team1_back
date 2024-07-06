b26546bdb0d3edfe3021337867f0f6ba42de336a
Kim Kyeongho <sgn07124@gmail.com>
2024-07-07 04:00:05 +0900

    BaseEntity 적용 되도록 수정

--- src/main/java/com/example/swyp_team1_back/SwypTeam1BackApplication.java
+++ src/main/java/com/example/swyp_team1_back/SwypTeam1BackApplication.java
@@ -2,8 +2,10 @@ package com.example.swyp_team1_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SwypTeam1BackApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/swyp_team1_back/global/common/entity/BaseTimeEntity.java
+++ src/main/java/com/example/swyp_team1_back/global/common/entity/BaseTimeEntity.java
@@ -4,15 +4,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@EntityListeners(value = AuditingEntityListener.class)
+@EntityListeners(value = {AuditingEntityListener.class})
 @MappedSuperclass
 @Getter
+@Setter
 public abstract class BaseTimeEntity {  // 작성 시간, 수정 시간 ..
 
     @CreatedDate
