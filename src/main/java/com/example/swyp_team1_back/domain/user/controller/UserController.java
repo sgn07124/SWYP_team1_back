cd2da8bf331261917479d9948018f5150f00ca96
Kim Kyeongho <sgn07124@gmail.com>
2024-07-07 17:21:22 +0900

    swagger 작성

--- src/main/java/com/example/swyp_team1_back/domain/user/controller/UserController.java
+++ src/main/java/com/example/swyp_team1_back/domain/user/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.swyp_team1_back.domain.user.controller;
 import com.example.swyp_team1_back.domain.user.dto.CreateUserDTO;
 import com.example.swyp_team1_back.domain.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +28,16 @@ public class UserController {
 
     @PostMapping("/signup")
     @Operation(summary = "일반 회원가입", description = "회원은 이메일로 일반 회원가입을 한다.")
+    @Parameters({
+            @Parameter(name = "email", description = "이메일 형식이어야 합니다.", example = "test1@naver.com"),
+            @Parameter(name = "password", description = "비밀번호는 최소 8자리 이상이어야 합니다.", example = "abcd1234"),
+            @Parameter(name = "rePassword", description = "비밀번호 재확인은 필수 항목입니다."),
+            @Parameter(name = "name", description = "이름은 한글 또는 영문으로 입력해야 하며, 한글 자음과 모음만 입력할 수 없고, 한글과 영문을 동시에 입력할 수 없습니다."),
+            @Parameter(name = "phone", description = "전화번호는 010-****-**** 형식으로 입력해야 합니다. *에는 숫자만 올 수 있습니다."),
+            @Parameter(name = "agreeTOS", description = "이용약관 동의는 필수 항목입니다.", example = "true"),
+            @Parameter(name = "agreePICU", description = "개인정보 처리방침 동의는 필수 항목입니다.", example = "true"),
+            @Parameter(name = "agreeMarketing", description = "마케팅 이메일 수신 동의는 선택 항목입니다.", example = "true or false"),
+    })
     public ResponseEntity<String> signUp(@RequestBody @Valid CreateUserDTO dto) {
         userService.signUp(dto);
         return ResponseEntity.ok("회원가입 성공!");
