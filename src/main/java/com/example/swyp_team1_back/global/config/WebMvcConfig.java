639e831a6f547bf91402c0d87e195954c4c45c1d
Kim Kyeongho <sgn07124@gmail.com>
2024-07-17 23:14:45 +0900

    WebMvcConfigurer 추가

---
+++ src/main/java/com/example/swyp_team1_back/global/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.example.swyp_team1_back.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://swyg-front.vercel.app/" , "http://15.164.202.203:8080/","http://localhost:3000")
+                .allowedMethods("OPTIONS","GET","POST","PUT","DELETE","PATCH");
+    }
+}
