53e1a9fccbedb465b2f761e9cc9bde8742bdc920
Kim Kyeongho <sgn07124@gmail.com>
2024-08-23 21:51:38 +0900

    수정

--- src/main/java/com/example/swyp_team1_back/global/config/SecurityConfig.java
+++ src/main/java/com/example/swyp_team1_back/global/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
@@ -98,6 +99,12 @@ public class SecurityConfig {
                         .deleteCookies("remember-me"));
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers("/error", "/favicon.ico");
     }
 
     @Bean
